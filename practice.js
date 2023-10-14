2096037c6d643a0f1975c5509d487dd70d1cb7a1
NimraFatima <farimanim16@gmail.com>
2023-10-13 21:32:24 -0600

    Initial commit

---
+++ practice.js
@@ -0,0 +1,69 @@
+
+// Practice2.1
+
+// let str1 = 'Laurence';
+// console.log(str1)
+
+
+// let str2 = "Svekis";
+// console.log(str2)
+
+// // let val1 = nimra;
+// // console.log(val1)
+
+// let val2 = null;
+// console.log(val2)
+
+// let myNum = 1000;
+// console.log(myNum)
+
+// Practice2.2
+
+// let myName = "My name is NImra";
+// let age = "I'm 23 years old";
+// let result = "I can do this assignment";
+// let d = myName + " " + age + " "+ result;
+// console.log(d);
+
+
+// let nr1 = 2;
+// let nr2 = 3;
+// let result1 = nr1 ** nr2;
+// console.log(result1);
+
+// practice 2.3
+
+// let a;
+// let b;
+// let c;
+// a = window.prompt("Enter value a");
+// a = Number(a);
+
+// b = window.prompt("Enter value b");
+// b = Number(b);
+// c = Math.sqrt (Math.pow(a,2) + Math.pow(b,2));
+// alert(c);
+
+// Practice 2.4
+
+
+// let a = 10; 
+// let b = 3;
+// let c = 5;
+
+// let add = b+a;
+// console.log(add)
+
+// let Divide = a/c;
+// console.log(Divide)
+
+// let d = c%b;
+// console.log(d)
+
+// Chapter Project
+
+// let a = 130;
+// let b = a*1.60934;
+// let c = "the distance of" + " " + a + " " + "kms is equal to " + " " + b + " " + "miles" 
+// console.log(c)
+
